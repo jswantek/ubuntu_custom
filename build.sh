8dc257a1bb938de858fc5b01b2a00269fc290dc9
Joe Swantek <joseph.swantek@northwestern.edu>
2020-06-19 12:54:15 -0500

    Build 18.04 and 20.04 custom images & add lsb-release

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
-#!
+#!/bin/bash
 
-docker build . --pull -f docker/Dockerfile \
-    -t ubuntu:focal_custom
+docker build . --pull -f docker/Dockerfile.18.04 \
+    -t ubuntu:18.04_custom
+
+docker build . --pull -f docker/Dockerfile.20.04 \
+    -t ubuntu:20.04_custom
